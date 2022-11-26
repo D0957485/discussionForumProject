e843a7788cd653697214a264022f7355964802a5
D0957485 <K24700643@gmail.com>
2022-11-27 01:36:03 +0800

    Kevin_update1127

---
+++ Mywebapp/Mywebapp/Mywebapp/Mywebapp/src/main/java/com/mycompany/user/artical.java
@@ -0,0 +1,53 @@
+package com.mycompany.user;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "articalTable")
+public class artical {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(length = 45, nullable = false, name = "topic")
+  private String topic;
+
+  @Column(length = 45, nullable = false, name = "content")
+  private String content;
+
+  @Column(length = 45, nullable = false, name = "from")
+  private String from;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getFrom() {
+    return from;
+  }
+
+  public void setFrom(String from) {
+    this.from = from;
+  }
+}
